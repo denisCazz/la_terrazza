d2cf3a7e121d4b77e261c2d4314358ef920ed0f9
Denis Cazzulo <deniscazzulo@icloud.com>
2025-11-24 16:49:17 +0100

    ottimizzazioni seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,16 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
+import playformCompress from '@playform/compress';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://laterrazzacarmagnola.it',
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [sitemap(), playformCompress()]
 });
